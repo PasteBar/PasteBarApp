27de9d3a1b48e54c28b56ea0dd54380c493916e7
Sergey Kurdin <kurdin@gmail.com>
2024-05-23 22:25:03 -0400

    added new way to run powershell to get hardware id on windows

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -100,6 +100,33 @@ fn open_path_or_app(path: String) -> Result<(), String> {
 
 #[tauri::command]
 fn get_device_id() -> Result<String, String> {
+  #[cfg(target_os = "windows")]
+  {
+    let combined_output = Command::new("powershell").args([
+             "-WindowStyle",
+             "Hidden",
+             "-command",
+             r#"
+             $csproduct = Get-WmiObject Win32_ComputerSystemProduct | Select-Object -ExpandProperty UUID;
+             $bios = Get-WmiObject Win32_BIOS | Select-Object -ExpandProperty SerialNumber;
+             $baseboard = Get-WmiObject Win32_BaseBoard | Select-Object -ExpandProperty SerialNumber;
+             $cpu = Get-WmiObject Win32_Processor | Select-Object -ExpandProperty ProcessorId;
+             "$csproduct|$bios|$baseboard|$cpu"
+             "#,
+      ]).spawn().unwrap_or(String::new());
+
+    if combined_output.is_empty() {
+      return Err(MIDError::ResultMidError);
+    }
+
+    Ok(
+      combined_output
+        .trim()
+        .trim_start_matches('|')
+        .trim_end_matches('|')
+        .to_lowercase(),
+    )
+  }
   match mid::get("PasteBarApp") {
     Ok(id) => {
       debug_output(|| {
