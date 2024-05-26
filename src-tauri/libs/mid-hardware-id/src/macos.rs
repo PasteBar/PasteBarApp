f89da1d4188cdd8fce7b6222815f4159b15954a6
Sergey Kurdin <kurdin@gmail.com>
2024-05-26 01:22:45 -0400

    updated code for windows and mac hardware uuid

--- src-tauri/libs/mid-hardware-id/src/macos.rs
+++ src-tauri/libs/mid-hardware-id/src/macos.rs
@@ -6,64 +6,22 @@ use std::process::{Command, Stdio};
 
 #[cfg(target_os = "macos")]
 pub(crate) fn get_mid_result() -> Result<String, MIDError> {
-  let combined_output = Command::new("sh")
+  let output = Command::new("sh")
     .arg("-c")
-    .arg(r#"system_profiler SPHardwareDataType SPSecureElementDataType"#)
+    .arg("system_profiler SPHardwareDataType | awk '/Hardware UUID/ { print $3; }'")
     .stderr(Stdio::piped())
     .stdout(Stdio::piped())
-    .spawn();
+    .output()
+    .map_err(|_| MIDError::ResultMidError)?;
 
-  match combined_output {
-    Ok(output) => {
-      let output = output.wait_with_output().unwrap();
-      if !output.status.success() {
-        return Err(MIDError::ResultMidError);
-      }
-      let system_profiler_output = String::from_utf8_lossy(&output.stdout);
-      if system_profiler_output.is_empty() {
-        return Err(MIDError::ResultMidError);
-      }
-
-      let targets = [
-        "Model Number",
-        "Serial Number",
-        "Hardware UUID",
-        "Provisioning UDID",
-        "Platform ID",
-        "SEID",
-      ];
-
-      let combined_string = process_output(&system_profiler_output, &targets);
-
-      if combined_string.is_empty() {
-        return Err(MIDError::ResultMidError);
-      }
-
-      Ok(combined_string)
-    }
-    Err(_e) => Err(MIDError::ResultMidError),
-  }
-}
-
-#[cfg(target_os = "macos")]
-fn process_output(output_str: &str, targets: &[&str]) -> String {
-  let mut result = Vec::new();
-
-  for target in targets {
-    for line in output_str.to_lowercase().lines() {
-      if line.contains(&target.to_lowercase()) {
-        let parts: Vec<&str> = line.split(":").collect();
-
-        if parts.len() == 2 {
-          let value = parts[1].trim().to_string();
-
-          if !value.is_empty() {
-            result.push(value);
-          }
-        }
-      }
-    }
+  if !output.status.success() {
+    return Err(MIDError::ResultMidError);
   }
 
-  result.join("|")
+  let uuid = String::from_utf8_lossy(&output.stdout).trim().to_string();
+  if uuid.is_empty() {
+    return Err(MIDError::ResultMidError);
+  }
+
+  Ok(uuid)
 }
--- src-tauri/libs/mid-hardware-id/src/windows.rs
+++ src-tauri/libs/mid-hardware-id/src/windows.rs
@@ -2,55 +2,29 @@
 use crate::errors::MIDError;
 
 #[cfg(target_os = "windows")]
-use std::os::windows::process::CommandExt;
+use serde::Deserialize;
+#[cfg(target_os = "windows")]
+use wmi::{COMLibrary, Variant, WMIConnection};
 
 #[cfg(target_os = "windows")]
-use std::process::Command;
-use std::process::Stdio;
-
-const CREATE_NO_WINDOW: u32 = 0x08000000;
+#[derive(Deserialize, Debug)]
+struct Win32_ComputerSystemProduct {
+  #[serde(rename = "UUID")]
+  uuid: String,
+}
 
 #[cfg(target_os = "windows")]
-pub(crate) fn get_mid_result() -> Result<String, MIDError> {
-  let combined_output = Command::new("powershell")
-    .args([
-      "-WindowStyle",
-      "Hidden",
-      "-command",
-      r#"
-        $csproduct = Get-WmiObject Win32_ComputerSystemProduct | Select-Object -ExpandProperty UUID;
-        $bios = Get-WmiObject Win32_BIOS | Select-Object -ExpandProperty SerialNumber;
-        $baseboard = Get-WmiObject Win32_BaseBoard | Select-Object -ExpandProperty SerialNumber;
-        $cpu = Get-WmiObject Win32_Processor | Select-Object -ExpandProperty ProcessorId;
-        "$csproduct|$bios|$baseboard|$cpu"
-        "#,
-    ])
-    .creation_flags(CREATE_NO_WINDOW)
-    .stderr(Stdio::piped())
-    .stdout(Stdio::piped())
-    .spawn();
+pub(crate) fn get_hardware_uuid() -> Result<String, MIDError> {
+  let com_con = COMLibrary::new().map_err(|_| MIDError::ResultMidError)?;
+  let wmi_con = WMIConnection::new(com_con.into()).map_err(|_| MIDError::ResultMidError)?;
 
-  match combined_output {
-    Ok(output) => {
-      let output = output.wait_with_output().unwrap();
-      if !output.status.success() {
-        return Err(MIDError::ResultMidError);
-      }
-      let combined_output = String::from_utf8_lossy(&output.stdout);
-      if combined_output.is_empty() {
-        return Err(MIDError::ResultMidError);
-      }
+  let results: Vec<Win32_ComputerSystemProduct> = wmi_con
+    .raw_query("SELECT UUID FROM Win32_ComputerSystemProduct")
+    .map_err(|_| MIDError::ResultMidError)?;
 
-      println!("Device ID: {}", combined_output.trim());
-
-      Ok(
-        combined_output
-          .trim()
-          .trim_start_matches('|')
-          .trim_end_matches('|')
-          .to_lowercase(),
-      )
-    }
-    Err(_e) => Err(MIDError::ResultMidError),
+  if let Some(product) = results.first() {
+    Ok(product.uuid.clone().to_lowercase())
+  } else {
+    Err(MIDError::ResultMidError)
   }
 }
