0871f6d63d02218b267e8d0a7875da1ef2b05bc3
Sergey Kurdin <kurdin@gmail.com>
2024-05-26 02:16:51 -0400

    update to hardware uuid detection on Windows

--- src-tauri/libs/mid-hardware-id/src/windows.rs
+++ src-tauri/libs/mid-hardware-id/src/windows.rs
@@ -1,29 +1,31 @@
 #[cfg(target_os = "windows")]
-use crate::errors::MIDError;
+use wmi::{COMLibrary, WMIConnection};
 
 #[cfg(target_os = "windows")]
 use serde::Deserialize;
-#[cfg(target_os = "windows")]
-use wmi::{COMLibrary, Variant, WMIConnection};
 
 #[cfg(target_os = "windows")]
-#[derive(Deserialize, Debug)]
-struct Win32_ComputerSystemProduct {
-  #[serde(rename = "UUID")]
-  uuid: String,
+use crate::errors::MIDError;
+
+#[cfg(target_os = "windows")]
+#[derive(Deserialize)]
+#[allow(non_snake_case)]
+struct UUIDGeneric {
+  UUID: String,
 }
 
 #[cfg(target_os = "windows")]
-pub(crate) fn get_hardware_uuid() -> Result<String, MIDError> {
-  let com_con = COMLibrary::new().map_err(|_| MIDError::ResultMidError)?;
-  let wmi_con = WMIConnection::new(com_con.into()).map_err(|_| MIDError::ResultMidError)?;
+pub fn get_mid_result() -> Result<String, MIDError> {
+  let com_connection = unsafe { COMLibrary::assume_initialized() };
+  let wmi_connection =
+    WMIConnection::new(com_connection.into()).map_err(|_| MIDError::ResultMidError)?;
 
-  let results: Vec<Win32_ComputerSystemProduct> = wmi_con
-    .raw_query("SELECT UUID FROM Win32_ComputerSystemProduct")
+  let computer_uuid_base: Vec<UUIDGeneric> = wmi_connection
+    .raw_query("SELECT UUID from Win32_ComputerSystemProduct WHERE UUID IS NOT NULL")
     .map_err(|_| MIDError::ResultMidError)?;
 
-  if let Some(product) = results.first() {
-    Ok(product.uuid.clone().to_lowercase())
+  if let Some(uuid_entry) = computer_uuid_base.first() {
+    Ok(uuid_entry.UUID.to_string())
   } else {
     Err(MIDError::ResultMidError)
   }
