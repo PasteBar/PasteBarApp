3dbd18386b12354b991f00a882b43c8eeb4f9311
Sergey Kurdin <kurdin@gmail.com>
2024-07-22 01:12:03 -0400

    added app store build and sign scripts

---
+++ build-universal-pkg.sh
@@ -0,0 +1,1 @@
+productbuild --component ./src-tauri/target/universal-apple-darwin/release/bundle/macos/PasteBar.app /Applications/ --sign "3E98D510E0A33BB08522EFB0B2B6990EED1E7BB7" Product-universal.pkg
---
+++ sign-universal.sh
@@ -0,0 +1,1 @@
+codesign --force -s E82B9F2DC3601C2E3120E95017D295E7E24898D9 -o runtime --entitlements entitlements_prod.plist ./src-tauri/target/universal-apple-darwin/release/bundle/macos/PasteBar.app
